b0f5bc57a863c34d8d0b352841c5fd3cda43badb
Heverson <heversondamasceno@gmail.com>
2021-09-14 17:53:43 -0300

    fix: config metro watch components

--- packages/apps/MyAppOne/metro.config.js
+++ packages/apps/MyAppOne/metro.config.js
@@ -1,4 +1,31 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require('expo/metro-config');
+const path = require('path');
 
-module.exports = getDefaultConfig(__dirname);
+const config = getDefaultConfig(__dirname);
+
+// monorepo config root node_modules 
+config.projectRoot = path.resolve(__dirname, '.');
+config.watchFolders = [
+    path.resolve(__dirname, '../../../node_modules'),
+    path.resolve(__dirname, '../../shared/components'),
+    path.resolve(__dirname),
+];
+
+// Remove all console logs in production...
+config.transformer.minifierConfig.compress.drop_console = true;
+
+
+config.resolver =  {
+    extraNodeModules: new Proxy(
+        {},
+        {
+            get: (target, name) => {
+                return path.join(__dirname, `node_modules/${name}`);
+            },
+        },
+    ),
+},
+  
+
+module.exports = config;
